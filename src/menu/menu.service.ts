2e038d5f9225a5599649fcf92ff30f904b51db6c
carlaarean79 <carlaandreaarean@gmail.com>
2023-08-27 21:44:32 -0300

    v-Get

--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -3,9 +3,9 @@ import { Menu } from './menu.interface';
 const BASE_URL = "http://localhost:3031/menu/"
 @Injectable()
 export class MenuService {
-   async getMenu(): Promise<Menu[]> {
-        const res = await fetch(BASE_URL);
-        const parsed = await res.json();
-        return parsed;    }
+    async getMenu(): Promise<Menu[]> {
+         const res = await fetch(BASE_URL);
+         const parsed = await res.json();
+         return parsed;    }
    
 }
