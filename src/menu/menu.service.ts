ac91d4cc4cb2f0433d7463f78d2c0b849f9d657c
andres-coda <codaandres@gmail.com>
2023-09-12 20:34:11 -0300

    resolución de querys

--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -9,62 +9,13 @@ export class MenuService {
   }
   async getMenuById(id: number): Promise<Menu> {
     const res = await fetch(BASE_URL + id);
-    const parsed = await res.json();
+    const parsed = await res.json();    
     return parsed;
   }
 
   async getMenu(): Promise<Menu[]> {
     const res = await fetch(BASE_URL);
     const parsed = await res.json();
-    return parsed;
-  }
-
-  async getMenuByTitle(title: string): Promise<Menu[]> {
-    const allMenu = await this.getMenu();
-    const filterByTitle = allMenu.filter((Menu) => Menu.title.toUpperCase().includes(title.toUpperCase()))
-    if (!filterByTitle.length) throw new NotFoundException({ messege: "No hay coincidencias" })
-    return filterByTitle;
-  }
-  
-  async getMenuByTipo(tipo: string): Promise<Menu[]> {
-    const allMenu = await this.getMenu();
-    const filterByTipo = allMenu.filter((Menu) => Menu.tipo.toUpperCase().includes(tipo.toUpperCase()))
-    if (!filterByTipo.length) throw new NotFoundException({ messege: "No hay coincidencias" })
-    return filterByTipo;
-  }
-
-  async getMenuByDescription(description: string): Promise<Menu[]> {
-    const allMenu = await this.getMenu();
-    const filterByDescription = allMenu.filter((Menu) => Menu.description.toUpperCase().includes(description.toUpperCase()))
-    if (!filterByDescription.length) throw new NotFoundException({ messege: "No hay coincidencias" })
-    return filterByDescription;
-  }
-
-  async getMenuByValoration(valoration: string): Promise<Menu[]> {
-    const allMenu = await this.getMenu();
-    const filterByValoration = allMenu.filter((Menu) => Menu.valoration.toUpperCase().includes(valoration.toUpperCase()))
-    if (!filterByValoration.length) throw new NotFoundException({ messege: "No hay coincidencias" })
-    return filterByValoration;
-}
-  async getMenuByCategory(category: string): Promise<Menu[]> {
-    const allMenu = await this.getMenu();
-    const filterByCategory = allMenu.filter((Menu) => Menu.category.toUpperCase().includes(category.toUpperCase()))
-    if (!filterByCategory.length) throw new NotFoundException({ messege: "No hay coincidencias" })
-    return filterByCategory;
-  }
-
-  async getMenuByIngredients(ingredients: string): Promise<Menu[]> {
-    const allMenu = await this.getMenu();
-    const filterByIngredients = allMenu.filter((Menu) => Menu.ingredients.toUpperCase().includes(ingredients.toUpperCase()))
-    if (!filterByIngredients.length) throw new NotFoundException({ messege: "No hay coincidencias" })
-    return filterByIngredients;
-
-  }
-
-  async getMenuByPrice( price: string ) : Promise <Menu[]>{
-    const res = await this.getMenu();
-    const parsed = res.filter((menu)=> Number(menu.price)=== Number(price));
-    if (!parsed.length) throw new NotFoundException(`No existe menu que contenga el precio ${price}`);
     return parsed;
   }
 
