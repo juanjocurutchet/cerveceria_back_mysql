d963d37b880e5176a9e16bcf87ad70e907f43446
FACUNDO-ALVAREZ-TORRIGLIA <facualvarezt@gmail.com>
2023-09-15 09:52:48 -0300

    Agregamos DTO a Users

---
+++ src/users/user.dto.ts
@@ -0,0 +1,15 @@
+import { IsString} from 'class-validator';
+export class UserDto {
+    @IsString()
+    name: string;
+    @IsString()
+    lastName: string;
+    @IsString()
+    user: string;
+    @IsString()
+    email: string;
+    @IsString()
+    password: string;
+    @IsString()
+    age: string;
+}
