83a9ec0e7456efaf66cf12e7c4aaa254d16b1b39
andres-coda <codaandres@gmail.com>
2023-09-19 23:13:01 -0300

    agregado de elementos db

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*' // Esto permite solicitudes desde cualquier origen
   });
-app.useGlobalPipes(new ValidationPipe({whitelist:true, forbidNonWhitelisted:true}))
+app.useGlobalPipes(new ValidationPipe({whitelist:true, /*forbidNonWhitelisted:true*/}))
   await app.listen(3000);
 }
 bootstrap();
